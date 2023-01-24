44eff6d3fa56dab4dd92189ae91c2a9e75e6e555
amoham1811 <mohammed_wok@yahoo.co.uk>
2023-01-24 22:13:24 +0000

    add starter files and questions

---
+++ assets/js/questions.js
@@ -0,0 +1,73 @@
+//question object created to hold questions and multiple choice answers and the correct answers
+var questions = [
+    {question1: "Is JavaScript a compiled or an interpreted language ?",
+    Choice1: "1. A compiled language",
+    Choice2: "2. An interpreted language",
+    Choice3: "3. Neither",
+    Choice4: "4. Both",
+    Answer: "2. JavaScript is an interpreted language"
+    },
+    {question2: "What extra tabs must be added to an HTML page to include JavaScript statement ?",
+    Choice1: "1. <script> and </script>",
+    Choice2: "2. <type='text/javascript'>",
+    Choice3: "3. <js> and </js>",
+    Choice4: "4. <!-- and -->",
+    Answer: "1. <script> and </script> must be added"
+    },
+    {question3: "The top level of the DOM hierarchy is occupied by",
+    Choice1: "1. The document property",
+    Choice2: "2. The document method",
+    Choice3: "3. The document object",
+    Choice4: "4. The window object",
+    Answer: "4. It is the window object"
+    },
+    {question4: "What is an onClick event handler ?",
+    Choice1: "1. An object that detects the mouse's location in a browser.",
+    Choice2: "2. A script that executes in response to the user clicking the mouse.",
+    Choice3: "3. An HTML element that the user can click.",
+    Choice4: "4. None of the above.",
+    Answer: "2. It is the script that executes on a mouse click."
+    },
+    {question5: "Which of these is not a true statement about variables ?",
+    Choice1: "1. Their names are case sensitive.",
+    Choice2: "2. They can contain numeric or non-numeric information",
+    Choice3: "3. Their names may contain spaces.",
+    Choice4: "4. Their values can be changed.",
+    Answer: "3. The names must not contain spaces"
+    },
+    {question6: "Which of these is a valid comment in a JavaScript program?",
+    Choice1: "1. // This is a comment.",
+    Choice2: "2. /* this is a comment */",
+    Choice3: "3. Both of the above.",
+    Choice4: "4. Neither of the above.",
+    Answer: "3. both are valid comments"
+    },
+    {question7: "Functions are called using",
+    Choice1: "1. The function keyword",
+    Choice2: "2. The call command",
+    Choice3: "3. The function name, with the parenthesis",
+    Choice4: "4. The function keyword, with parenthesis",
+    Answer: "3. functions are called using their name followed by parantensis"
+    },
+    {question8: "What happens when a function executes a return statement?",
+    Choice1: "1. An error message is generated.",
+    Choice2: "2. A value is returned and the function execution continues.",
+    Choice3: "3. A value is logged to the console if the return value is null.",
+    Choice4: "4. A value is returned and the function execution stops",
+    Answer: "4. A value is returned and the execution stops"
+    },
+    {question9: "Which method of the Math() object always rounds a number up to the next integer?",
+    Choice1: "1. Maths.round()",
+    Choice2: "2. Math.ceil()",
+    Choice3: "3. Maths.floor()",
+    Choice4: "4. Math.roundup()",
+    Answer: "2. The correct option was Math.ceil()"
+    },
+    {question10: "The innerHTML property of a <div> element can be used",
+    Choice1: "1. To set the HTML content of the div",
+    Choice2: "2. To get the HTML content of the div",
+    Choice3: "3. Either of the above",
+    Choice4: "4. Neither of the above",
+    Answer: "3. the innerHTML property can be used for both getting and setting HTML content"
+    }
+]
