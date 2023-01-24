cf82c9656fcc8b2f288036630fe9304c57f7239d
amoham1811 <mohammed_wok@yahoo.co.uk>
2023-01-24 22:55:16 +0000

    add timer and feedback

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,13 +4,53 @@ var choicesElement = document.querySelector("#choices");
 var buttonClk = document.querySelector(".start");
 var choiceClk = document.querySelector("#choices");
 var starter = document.querySelector("#start-screen");
+var timerEl = document.getElementById('time');
 var endScreen = document.getElementById('end-screen');
 var playerInitials = document.querySelector("#initials");
-
+var finalScore = document.querySelector("#final-score");
+var submitButton = document.getElementById('submit');
 //questionElement.textContent = questions[0].question1;
+var counter = 0
 var feedback = '';
 var quizScore = 0;
-var counter = 0;
+var timeLeft = 0
+
+function displayResult() {
+    questionElement.setAttribute("class","hide");
+    endScreen.setAttribute("class","start");
+    var finScoreEl = document.getElementById("final-score");
+    finScoreEl.textContent = quizScore+" out of 10";
+
+}
+// Timer that counts down from 60
+function countdown() {
+    timeLeft = 60;
+
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      // As long as the `timeLeft` is greater than 1
+      if (timeLeft > 1) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        // When `timeLeft` is equal to 1
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+        timerEl.textContent = 'Times Up!';
+        
+        //
+
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
+        // Call the `displayMessage()` function
+        displayResult();
+      }
+    }, 1000);
+  }
 
 function getFirstQuestion(){
         questionElement.textContent = Object.values(questions[counter])[0];
@@ -42,7 +82,18 @@ function getNextQuestion(){
         div.appendChild(button);
         div.textAlign = "left";
         questionElement.appendChild(div);
-    }              
+    }          
+    //return feedback
+    var pElem = document.createElement("p");
+    pElem.textContent = "_________________________________________________";
+    pElem.textAlign = "left";
+    questionElement.appendChild(pElem);
+    var pElem = document.createElement("p");
+    pElem.textContent = feedback;
+    pElem.textAlign = "left";
+    questionElement.appendChild(pElem);
+    
+    
     //alert(questionElement.textContent);
 }
 
@@ -65,13 +116,44 @@ buttonClk.addEventListener("click",function(event){
     }
     if (element.matches("button")== true){
         getFirstQuestion();
+        countdown();
     }
 })
 questionElement.addEventListener("click",function(event){
     var element = event.target;
-    if (counter <= 10){ 
+    if (counter <= 10 && timeLeft >= 0){
+        //alert(element);
+        if (element.matches("button")==true) {
+        if (element.textContent[0] === Object.values(questions[counter])[5][0]){
+                feedback = "Well done! That was the correct answer";
+                quizScore++;
+        }else {
+                feedback = "Incorrect! "+ Object.values(questions[counter])[5].substring(2,) ;
+                timeLeft -= 10;
+        } 
         clearQuestion(element)
         getNextQuestion(); 
+            // alert ("I am here");
+        }
+    }else{
+        displayResult();
+    }
+})
+
+submitButton.addEventListener("click",function(event){
+    event.preventDefault();
+    var scoreBoard = {
+        pInitial: playerInitials.value,
+        fScore: finalScore.value
+      };
+    // validate the fields
+    if (scoreBoard.pInitial === "") {
+        displayMessage("error", "Player initials cannot be blank");
+      } else {
+        alert("success", "Results recorded successfully");
+        // set new submission
+        localStorage.setItem("scoreBoard", JSON.stringify(scoreBoard));
+        window.location.href = 'highscores.html';
     }
     
 })
